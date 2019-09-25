b85b15a5f7e5f4d200aa009453a8e097c0b7a000
Gabriel Julio <38597996+GabsJJ@users.noreply.github.com>
2019-09-25 09:24:48 -0300

    Mudanças na página de controle

--- Android/Nixplorer/app/src/main/java/br/unicamp/cotuca/nixplorer/ControlesActivity.java
+++ Android/Nixplorer/app/src/main/java/br/unicamp/cotuca/nixplorer/ControlesActivity.java
@@ -1,9 +1,13 @@
 package br.unicamp.cotuca.nixplorer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class ControlesActivity extends AppCompatActivity {
 
@@ -11,11 +15,19 @@ public class ControlesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controles);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.controles, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
     }
 }
